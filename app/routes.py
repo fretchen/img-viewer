6a36d97039a93983784ebf95f72de420a7e5f6d9
Fred J <fred.jendrzejewski@gmail.com>
2019-11-15 08:38:54 +0100

    Move the image all into a folder

--- app/routes.py
+++ app/routes.py
@@ -2,22 +2,12 @@ from flask import render_template, send_from_directory, send_file
 from app import app
 import os
 
-from models import image_reg, Image
+from models import Image
 
 @app.route('/')
 @app.route('/index')
 def index():
-    path = app.config['IMAGES_FOLDER'];
-    image_reg = [];
-    for root, dirs, files in os.walk(app.config['IMAGES_FOLDER']):
-        for file in files:
-            if file.endswith(".png"):
-                rel_path = os.path.relpath(root, app.config['IMAGES_FOLDER']);
-                split_path = rel_path.split(os.sep);
-                image = Image(os.path.join(rel_path, file), int(split_path[0]),
-                    int(split_path[1]), int(split_path[2]));
-                image_reg.append(image)
-
+    image_reg = Image.all(app.config['IMAGES_FOLDER']);
     return render_template('index.html', title='Home', user='Miguel', images = image_reg)
 
 @app.route('/cdn/<path:filename>')
--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-image_reg  = []
+import os
 
 class Image:
     def __init__(self, path, year = 0, month = 0, day = 0):
@@ -6,3 +6,18 @@ class Image:
         self.year = year;
         self.month = month;
         self.day = day;
+
+    @classmethod
+    def all(cls, img_folder):
+        """Return a list of files contained in the directory pointed by settings.GALLERY_ROOT_DIR.
+        """
+        image_reg = [];
+        for root, dirs, files in os.walk(img_folder):
+            for file in files:
+                if file.endswith(".png"):
+                    rel_path = os.path.relpath(root, img_folder);
+                    split_path = rel_path.split(os.sep);
+                    image = cls(os.path.join(rel_path, file), int(split_path[0]),
+                        int(split_path[1]), int(split_path[2]));
+                    image_reg.append(image)
+        return image_reg;
