e3f13bd24ab19bed6846e34e08125ef5fc8d6043
Fred J <fred.jendrzejewski@gmail.com>
2019-11-15 07:35:39 +0100

    Pulling out the data information into an object

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,8 @@
 from flask import render_template, send_from_directory, send_file
 from app import app
 import os
-import glob
 
-
-image_dir = '../../'
+from models import image_reg, Image
 
 @app.route('/')
 @app.route('/index')
@@ -18,12 +16,11 @@ def index():
             if file.endswith(".png"):
                 rel_path = os.path.relpath(root, app.config['IMAGES_FOLDER']);
                 split_path = rel_path.split(os.sep);
-                images.append({'image_name': os.path.join(rel_path, file),
-                'year':int(split_path[0]), 'month':int(split_path[1]),
-                'day': int(split_path[2])})
-                rel_paths.append(rel_path)
+                image = Image(os.path.join(rel_path, file), int(split_path[0]),
+                    int(split_path[1]), int(split_path[2]));
+                image_reg.append(image)
 
-    return render_template('index.html', title='Home', user='Miguel', images = reversed(images))
+    return render_template('index.html', title='Home', user='Miguel', images = image_reg)
 
 @app.route('/cdn/<path:filename>')
 def custom_static(filename):
---
+++ models.py
@@ -0,0 +1,8 @@
+image_reg  = []
+
+class Image:
+    def __init__(self, path, year = 0, month = 0, day = 0):
+        self.path = path;
+        self.year = year;
+        self.month = month;
+        self.day = day;
