edfd58f81edd22e10d1a21486ec3d4a2fe00b65b
Fred J <fred.jendrzejewski@gmail.com>
2019-11-22 13:33:16 +0100

    Protect from external views

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,12 @@
 from flask import render_template, send_from_directory, send_file, redirect, url_for
-from flask import request
+from flask import request, flash
 from app import app, db
 from datetime import datetime
 import os
 
-from app.models import ImageDB
-from flask_login import login_required
+from app.models import ImageDB, User
+from app.forms import LoginForm
+from flask_login import login_required, current_user, logout_user, login_user
 
 @app.route('/')
 @app.route('/index')
@@ -129,4 +130,25 @@ def add_images():
                         pass
         db.session.commit()
 
+    return redirect(url_for('index'))
+
+# user registration etc
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
     return redirect(url_for('index'))
