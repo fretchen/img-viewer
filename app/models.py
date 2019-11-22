87f88e4896b6d4811e74bc08b357802cd54735d2
Fred J <fred.jendrzejewski@gmail.com>
2019-11-22 10:59:00 +0100

    Add the user model

--- app/models.py
+++ app/models.py
@@ -21,3 +21,12 @@ class ImageDB(db.Model):
 
     def __repr__(self):
         return '<ImageDB {}>'.format(self.path);
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
